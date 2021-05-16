aef4ca09d867a93b59ef75f9e2cebf39f8225f3d
Maciej Korczala <maciej.coders@gmail.com>
2021-05-16 17:09:40 +0200

    Exercises - methods, task 8

--- src/main/java/pl/coderslab/methods/Main08.java
+++ src/main/java/pl/coderslab/methods/Main08.java
@@ -3,5 +3,19 @@ package pl.coderslab.methods;
 public class Main08 {
 
     public static void main(String[] args) {
+        double a = 47;
+        double b = 47.00001;
+        double c = 47;
+        double resultOfMethod = maxOfThree(a, b, c);
+        System.out.println("Największa podana liczba to: " + resultOfMethod);
+    }
+
+    public static double maxOfThree(double a, double b, double c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        } else
+            return c;
     }
 }
