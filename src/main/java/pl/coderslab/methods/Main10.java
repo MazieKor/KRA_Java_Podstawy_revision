6a60c35e6032bad54ee0274d4b5a7eb612aa8c31
Maciej Korczala <maciej.coders@gmail.com>
2021-05-16 17:55:50 +0200

    SSH test

--- src/main/java/pl/coderslab/methods/Main10.java
+++ src/main/java/pl/coderslab/methods/Main10.java
@@ -17,7 +17,6 @@ public class Main10 {
             System.out.println("Padł remis");
         } else
             System.out.println("Błąd");
-
     }
 
     public static String footballWin(int goalsATeamHome, int goalsATeamVisit, int goalsBTeamHome, int goalsBTeamVisit) {
