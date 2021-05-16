af328c586d7797cb51dfc0b79c6cdefe07c89a6e
Maciej Korczala <maciej.coders@gmail.com>
2021-05-16 17:10:12 +0200

    Exercises - methods, part 2

--- src/main/java/pl/coderslab/methods/Main09.java
+++ src/main/java/pl/coderslab/methods/Main09.java
@@ -1,7 +1,27 @@
-package pl.coderslab.strings;
+package pl.coderslab.methods;
 
 public class Main09 {
 
     public static void main(String[] args) {
+        int number = -4;
+        int factorial = factorial(number);
+
+        if (factorial < 0) {
+            System.out.println("Podaj liczbę większą lub równą zeru");
+        } else {
+            System.out.println("Silnia z liczby " + number + " wynosi: " + factorial);
+        }
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+        if (n >= 0) {
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+            return result;
+        } else {
+            return -1;
+        }
     }
 }
--- src/main/java/pl/coderslab/methods/Main10.java
+++ src/main/java/pl/coderslab/methods/Main10.java
@@ -3,5 +3,31 @@ package pl.coderslab.methods;
 public class Main10 {
 
     public static void main(String[] args) {
+        int goalsATeamHome = 1;
+        int goalsATeamVisit = 5;
+        int goalsBTeamHome = 0;
+        int goalsBTeamVisit = 6;
+
+        String winner = footballWin(goalsATeamHome, goalsATeamVisit, goalsBTeamHome, goalsBTeamVisit);
+        if (winner == "1") {
+            System.out.println("Zwycięzcą jest zespół A");
+        } else if (winner == "2") {
+            System.out.println("Zwycięzcą jest zespół B");
+        } else if (winner == "X") {
+            System.out.println("Padł remis");
+        } else
+            System.out.println("Błąd");
+
+    }
+
+    public static String footballWin(int goalsATeamHome, int goalsATeamVisit, int goalsBTeamHome, int goalsBTeamVisit) {
+        int goalsATeam = goalsATeamHome + goalsATeamVisit;
+        int goalsBTeam = goalsBTeamHome + goalsBTeamVisit;
+        if (goalsATeam > goalsBTeam || (goalsATeam == goalsBTeam && goalsATeamVisit > goalsBTeamVisit)) {
+            return "1";
+        } else if (goalsBTeam > goalsATeam || (goalsATeam == goalsBTeam && goalsBTeamVisit > goalsATeamVisit)) {
+            return "2";
+        } else
+            return "X";
     }
 }
