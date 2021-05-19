8cafb59ce55a17ddef12f7af3ad904e62cbecbad
Maciej Korczala <maciej.coders@gmail.com>
2021-05-19 16:11:05 +0200

    multiarrays - exercises

--- src/main/java/pl/coderslab/strings/Main01.java
+++ src/main/java/pl/coderslab/strings/Main01.java
@@ -1,10 +1,19 @@
 package pl.coderslab.strings;
 
+import java.util.Locale;
+
 public class Main01 {
 
     public static void main(String[] args) {
 
     String str = "CodersLab jak dobrze zacząć programować";
+
+
+    String b = "adam";
+    b = b.toUpperCase();
+
+        System.out.println(b);
+
 
     }
 }
