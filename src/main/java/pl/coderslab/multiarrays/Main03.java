2ba8bb3531c246866fc64df0917d6c82730031e6
Maciej Korczala <maciej.coders@gmail.com>
2021-05-21 00:08:21 +0200

    multiarrays exercises - improvements

--- src/main/java/pl/coderslab/multiarrays/Main03.java
+++ src/main/java/pl/coderslab/multiarrays/Main03.java
@@ -5,6 +5,7 @@ public class Main03 {
     public static void main(String[] args) {
         int[][] task3Array = {{2, 3, 4, 6}, {12, 32, 12, 11}, {3, 2, 1, 4, 5}, {5, 1, 6, 7, 8}};
 
+//1st solution:
         int sum = 0;
         for (int i = 0; i < task3Array.length; i++) {
             for (int j = 0; j < task3Array[i].length; j++) {
@@ -12,5 +13,14 @@ public class Main03 {
             }
         }
         System.out.println("suma tablicy to " + sum);
+
+//2nd solution:
+        int sum2 = 0;
+        for (int[] row : task3Array) {
+            for (int element : row) {
+                sum2 += element;
+            }
+        }
+        System.out.println("2 sposób: suma tablicy to " + sum2);
     }
 }
--- src/main/java/pl/coderslab/multiarrays/Main04.java
+++ src/main/java/pl/coderslab/multiarrays/Main04.java
@@ -3,8 +3,11 @@ package pl.coderslab.multiarrays;
 public class Main04 {
 
     public static void main(String[] args) {
+
+//2 solutions:
         int[][] task4Array = {{2, 3, 4, 6}, {12, 32, 12, 11}, {3, 2, 1, 4}, {5, 1, 6, 7}};
 
+//1st solution:
         int sum = 0;
         for (int i = 0; i < task4Array.length; i++) {
             for (int j = 0; j < task4Array[i].length; j++) {
@@ -14,6 +17,22 @@ public class Main04 {
                 }
             }
         }
-        System.out.print("Slad macierzy to: " + sum);
+        System.out.println("Slad macierzy to: " + sum);
+
+//2nd solution:
+        int sum2 = 0;
+        int counter = 0;
+        for(int[] row : task4Array){
+            int counter2 = 0;
+            for(int value : row){
+                if(counter==counter2){
+                    sum2 = sum2 + value;
+                    break;
+                }
+            counter2++;
+            }
+        counter++;
+        }
+        System.out.print("2 sposob: Slad macierzy to: " + sum2);
     }
 }
