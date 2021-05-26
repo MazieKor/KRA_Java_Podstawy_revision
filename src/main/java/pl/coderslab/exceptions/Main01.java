73b4905f6212278bc6e0e12145808283ed9d2102
Maciej Korczala <maciej.coders@gmail.com>
2021-05-26 23:58:53 +0200

    exercises - exeptions

--- src/main/java/pl/coderslab/exceptions/Main01.java
+++ src/main/java/pl/coderslab/exceptions/Main01.java
@@ -4,11 +4,21 @@ package pl.coderslab.exceptions;
 public class Main01 {
 
     public static void main(String[] args) {
-
+        int number = 3;
+        try {
+        int result = factorial(number);
+        System.out.println("silnia z liczby " + number + " wynosi " + result);
+        } catch(IllegalArgumentException e) {
+         e.printStackTrace();
+        }
+        System.out.println("wykonaj dalsze działania");
     }
 
     static int factorial(int number) {
         int result = 1;
+        if (number < 0) {
+            throw new IllegalArgumentException("podaj liczbę większą lub równą zeru");
+        }
         for (int i = 1; i <= number; i++) {
             result *= i;
         }
